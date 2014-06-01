d248b64b95c26811080fdf9b9cbd87b5be29f745
arijit-pande <pande.arijit@gmail.com>
2014-06-01 04:44:05 -0400

    updated script reads date,and facebook key

--- final_script.py
+++ final_script.py
@@ -1,15 +1,39 @@
 from facebookapp import *
+from Helper import *
 import sys
 from PyQt4 import QtCore, QtGui
 
-def main():
-    app = QtGui.QApplication(sys.argv)
-    MainWindow = QtGui.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+class Facebook():
     
+    def start(self):
+        self.MainWindow=QtGui.QMainWindow()
+        self.ui=Ui_MainWindow()
+        self.ui.setupUi(self.MainWindow)
+        self.ui.help.clicked.connect(self.help_screen)
+        self.ui.submit.clicked.connect(self.submit_button)
+        self.MainWindow.show()
+    
+    def help_screen(self):  #shows help screen on click
+        self.Form=QtGui.QWidget()
+        ui1=Ui_Form()
+        ui1.setupUi(self.Form)
+        self.Form.show()
+
+    
+    def submit_button(self):
+        self.key=str(self.ui.keytext.toPlainText())
+        self.msg_p=str(self.ui.prefix_msg.toPlainText())
+        self.msg_s=str(self.ui.suffix_msg.toPlainText())
+        self.date1=str(self.ui.dateEdit.date())
+        print self.key,self.msg_p,self.msg_s,self.date1
+        #Facebook connectivity code to be added here
+
+def main():
+    app=QtGui.QApplication(sys.argv)
+    F=Facebook()
+    F.start()
+    sys.exit(app.exec_())
+
 
 if __name__=='__main__':
     main()
